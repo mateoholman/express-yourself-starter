1d347d1138a27048e0218e104f8aa753e6a5aefc
Matthew Holman <mateoholman@gmail.com>
2016-10-15 21:46:10 -0500

    Finished js/ejs and added some styling

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const path = require('path');
 // dependencies from npm (package.json)
 const express = require('express');
 const ejs = require('ejs');
+const ghAvatar = require('gh-avatar');
 
 // Initialize our app
 const app = express();
+
+var avatarURL = 'https://avatars.githubusercontent.com/u/12854015?v=3';
+
+// Initialize our GH avatar
+ghAvatar('mateoholman').then(avatar => {
+    console.log(avatar);
+});
 
 // Set our views directory
 app.use(express.static('css'));
@@ -15,7 +23,18 @@ app.set('view engine', 'ejs');
 
 // Add a route to our app that renders our index view
 app.get('/', function(req, res, next) {
-  res.render('index.ejs', { names: ['Rick', 'Morty', 'Summer', 'Beth', 'Jerry'] });
+  res.render('index.ejs', {
+    avatar: avatarURL,
+    greeting: 'Hi, my name is Matthew Holman. I live in Austin, TX and I am a web developer.',
+    weapons: {
+      title: 'Weapons Mastery',
+      items: ['katana', 'sai', 'shoryuken', 'shoe']
+    },
+    vehicles: {
+      title: 'Vehicle Mastery',
+      items: ['air ship', 'u-boat', 'roller blades']
+    }
+  });
 });
 
 // Set up our server
